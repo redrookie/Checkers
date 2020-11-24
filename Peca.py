1eead15c75672bd05513b7cb07cafb80a0d8f6e8
Bruno <bruno.bellisi@gmail.com>
2020-11-24 21:42:52 -0200

    Posiçôes das peças concertadas. Tipo das peças alterado para Sprite facilitando detecção de colisão

--- Peca.py
+++ Peca.py
@@ -1,13 +1,15 @@
 import pygame
 
+class Peca(pygame.sprite.Sprite):
 
-class Peca:
-    def __init__(self, cor, x, y):
+    def __init__(self, cor, pos):
+        super().__init__()
         self.cor = cor
-        self.x = x
-        self.y = y
-        self.img = None
-    
+        self.pos = pos
+        self.image = self.getImg()
+        self.rect = self.image.get_rect()
+        self.rect.topleft = pos
+
     def getImg(self):
         if self.cor == 'preta':
             self.img = pygame.image.load('img/black.png')
@@ -17,4 +19,6 @@ class Peca:
             self.img = pygame.image.load('img/white.png')
             self.img = pygame.transform.scale(self.img, (50,50))
             return self.img
-            
+
+    def update(self):
+        self.rect.topleft = self.pos
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import pygame_menu
-from Jogo import Jogo
+from Peca import Peca
 
 pygame.init()
 
@@ -9,14 +9,40 @@ board = pygame.image.load('img/checkers_board.jpg')
 screen = pygame.display.set_mode((520,520))
 pygame.display.set_caption("Damas")
 pygame.display.set_icon(icon)
-menu = pygame_menu.Menu(520, 520, 'Damas',theme=pygame_menu.themes.THEME_DARK)
+menu = pygame_menu.Menu(520, 520, 'Damas', onclose=pygame_menu.events.EXIT, theme=pygame_menu.themes.THEME_DARK)
 menu.add_button('Play', menu.disable)
 menu.add_button('Quit', pygame_menu.events.EXIT)
+
 #GameLoop
 running = True
-jogo = Jogo()
-mouse_pos = [(0,0),(0,0)] # Pos antiga e nova do click do mouse para movimentar as pecas
-count_clicks = 0 # Quando 2 clicks forem contados, movimenta a peca de mouse_pos[0] para mouse_pos[1]
+
+#Criar pecas
+black_pieces_group = pygame.sprite.Group()
+white_pieces_group = pygame.sprite.Group()
+black_pieces_group.add(Peca('preta', (0,0)))
+black_pieces_group.add(Peca('preta', (135,0)))
+black_pieces_group.add(Peca('preta', (265,0)))
+black_pieces_group.add(Peca('preta', (405,0)))
+black_pieces_group.add(Peca('preta', (65,67)))
+black_pieces_group.add(Peca('preta', (200,67)))
+black_pieces_group.add(Peca('preta', (340,67)))
+black_pieces_group.add(Peca('preta', (470,67)))
+black_pieces_group.add(Peca('preta', (0,135)))
+black_pieces_group.add(Peca('preta', (135,135)))
+black_pieces_group.add(Peca('preta', (265,135)))
+black_pieces_group.add(Peca('preta', (405,135)))
+white_pieces_group.add(Peca('branca', (62,465)))
+white_pieces_group.add(Peca('branca', (200,465)))
+white_pieces_group.add(Peca('branca', (335,465)))
+white_pieces_group.add(Peca('branca', (465,465)))
+white_pieces_group.add(Peca('branca', (0,410)))
+white_pieces_group.add(Peca('branca', (135,410)))
+white_pieces_group.add(Peca('branca', (265,410)))
+white_pieces_group.add(Peca('branca', (400,410)))
+white_pieces_group.add(Peca('branca', (62,340)))
+white_pieces_group.add(Peca('branca', (200,340)))
+white_pieces_group.add(Peca('branca', (335,340)))
+white_pieces_group.add(Peca('branca', (465,340)))
 
 while running:
     events = pygame.event.get()
@@ -24,15 +50,28 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONUP and not menu.is_enabled():
-            pos = pygame.mouse.get_pos()
-            count_clicks += 1
-            if count_clicks == 1:
-                mouse_pos[0] = pos
-            elif count_clicks == 2:
-                mouse_pos[1] = pos
-                count_clicks = 0
-                jogo.movimenta_peca(screen, mouse_pos, board)
-    #Exception ocorre ao dar Play no jogo pois o IF ainda esta rodando (CONSERTAR DEPOIS)
+            for p in black_pieces_group.sprites():
+                if p.rect.collidepoint(event.pos):
+                    position = p.__getattribute__('pos')
+                    pos = list(position)
+                    pos[0] += 15
+                    pos[1] += 15
+                    position = tuple(pos)
+                    p.__setattr__('pos', position)
+                else:
+                    pass
+
+            for p in white_pieces_group.sprites():
+                if p.rect.collidepoint(event.pos):
+                    position = p.__getattribute__('pos')
+                    pos = list(position)
+                    pos[0] += 15
+                    pos[1] += 15
+                    position = tuple(pos)
+                    p.__setattr__('pos', position)
+                else:
+                    pass
+
     if menu.is_enabled():
         try:
             menu.update(events)
@@ -40,5 +79,10 @@ while running:
         except:
             pass
     else:
-        jogo.desenha_tabuleiro(screen, board)
+        screen.blit(board, (0,0))
+        white_pieces_group.draw(screen)
+        black_pieces_group.draw(screen)
+        black_pieces_group.update()
+        white_pieces_group.update()
+
     pygame.display.update()
